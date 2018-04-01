b107135283b7179f00cac5c32c40391968f7c4f4
Шарапов Аркадий <FireCat1987@gmail.com>
2018-04-02 01:17:27 +0300

    Заглушил навремя тесты.

--- src/main/kotlin/nejen/Application.kt
+++ src/main/kotlin/nejen/Application.kt
@@ -2,11 +2,10 @@ package nejen
 
 import nejen.models.*
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.SchemaUtils.create
 import org.jetbrains.exposed.sql.StdOutSqlLogger
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.joda.time.DateTime
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -26,46 +25,7 @@ class Application {
             TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
             transaction {
                 logger.addLogger(StdOutSqlLogger)
-                SchemaUtils.drop(TenantsTable, RentsTable, ActTable, ShelfTable, OrdersTable, CashTable)
-                SchemaUtils.create(TenantsTable, RentsTable, ActTable, ShelfTable, OrdersTable, CashTable)
-                val tenant = TenantsItem.new {
-                    name = "One"
-                    telefone = "111111111"
-                    email = "One@One.ru"
-                }
-                val rent = RentsItem.new {
-                    number = "1"
-                    start_date = DateTime()
-                    stop_date = DateTime()
-                    tenants = tenant
-                }
-                val shelfi = ShelfItem.new {
-                    name = "Arkadiy"
-                    price = 110
-                }
-                val acti = ActItem.new {
-                    rents = rent
-                    shelf = shelfi
-                    start_date = DateTime()
-                    stop_date = DateTime()
-                    term = "OK"
-                    payment = "TRUE"
-                }
-                val order = OrdersItem.new {
-                    act = acti
-                    name_item = "PAKET"
-                    desc_item = "BOLSHOY PAKET"
-                    qty = "2"
-                    price = "220"
-                }
-                val cash = CashItem.new {
-                    cash_date = DateTime()
-                    orders = order
-                    sell = "3"
-                    take = "2"
-                    discount = "1"
-                }
-                commit()
+                create(TenantsTable, RentsTable, ActTable, ShelfTable, OrdersTable, CashTable)
             }
         }
     }
